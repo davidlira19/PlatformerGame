a7c7d195916f56bd0be04c8fc0f10e43d3148fdf
adrianam4 <61042878+adrianam4@users.noreply.github.com>
2020-10-27 13:25:32 +0100

    camera limits and more

--- Game/Source/Scene.cpp
+++ Game/Source/Scene.cpp
@@ -32,14 +32,14 @@ bool Scene::Awake()
 bool Scene::Start()
 {
 	// L03: DONE: Load map
-	//app->map->Load("hello2.tmx");
-	//app->map->Load("iso.tmx");
 	app->map->Load("snow_tileset.tmx");
-
 	// Load music
 	app->audio->PlayMusic("Assets/audio/music/Christmas_music2004.ogg");
+	//Load Position
 	app->render->camera.x = 0;
 	app->render->camera.y = -700;
+	//Load Texture
+	bg_snow = app->tex->Load("Assets/textures/snow_background.png");
 	return true;
 }
 
@@ -60,18 +60,39 @@ bool Scene::Update(float dt)
 		app->SaveGameRequest();
 
 	if (app->input->GetKey(SDL_SCANCODE_UP) == KEY_REPEAT)
-		app->render->camera.y -= 2;
+		app->render->camera.y -= 5;
 
 	if (app->input->GetKey(SDL_SCANCODE_DOWN) == KEY_REPEAT)
-		app->render->camera.y += 2;
+		app->render->camera.y += 5;
 
 	if (app->input->GetKey(SDL_SCANCODE_LEFT) == KEY_REPEAT)
-		app->render->camera.x -= 2;
+		app->render->camera.x -= 5;
 
 	if (app->input->GetKey(SDL_SCANCODE_RIGHT) == KEY_REPEAT)
-		app->render->camera.x += 2;
+		app->render->camera.x += 5;
 
-	//app->render->DrawTexture(img, 380, 100);
+	//CAMERA.X LIMITS
+	if (app->render->camera.x > 0)
+	{
+		app->render->camera.x = 0;
+	}
+	else if (app->render->camera.x <= -9000)
+	{
+		app->render->camera.x = -9000;
+	}
+	//CAMERA.Y LIMITS
+	if (app->render->camera.y >= -5)
+	{
+		app->render->camera.y = -5;
+	}
+	else if (app->render->camera.y <= -1100)
+	{
+		app->render->camera.y = -1100;
+	}
+	//DRAW BACKGROUND
+	app->render->DrawTexture(bg_snow, 0, 0);
+	app->render->DrawTexture(bg_snow, 3600, 0);
+	app->render->DrawTexture(bg_snow, 7202, 0);
 	app->map->Draw();
 	// L03: DONE 7: Set the window title with map/tileset info
 	
@@ -81,7 +102,7 @@ bool Scene::Update(float dt)
 		app->map->data.tilesets.count());
 
 	app->win->SetTitle(title.GetString());
-	
+	LOG("Position x: %d ------ Position y: %d", app->render->camera.x, app->render->camera.y);
 
 	return true;
 }
--- Game/Source/Scene.h
+++ Game/Source/Scene.h
@@ -34,6 +34,7 @@ public:
 
 private:
 	SDL_Texture* img;
+	SDL_Texture* bg_snow;
 };
 
 #endif // __SCENE_H__
