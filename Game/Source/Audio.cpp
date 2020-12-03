08e077d113af649c8d59463be4d1335185cf24d4
David Lira <davidlira1909@gmail.com>
2020-12-03 20:35:43 +0100

    music fix

--- Game/Source/Audio.cpp
+++ Game/Source/Audio.cpp
@@ -107,7 +107,7 @@ bool Audio::CleanUp()
 
 	if(music != NULL)
 	{
-		//Mix_FreeMusic(music);
+		Mix_FreeMusic(music);
 	}
 
 	ListItem<Mix_Chunk*>* item;
@@ -143,7 +143,7 @@ bool Audio::PlayMusic(const char* path, float fade_time)
 		}
 
 		// this call blocks until fade out is done
-		//Mix_FreeMusic(music);
+		Mix_FreeMusic(music);
 	}
 
 	music = Mix_LoadMUS(path);
